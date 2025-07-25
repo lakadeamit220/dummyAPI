abe196690732531aed58c33ae83e510b840b76d6
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-25 18:24:24 +0530

    Implemented total air cleaned from Yuka Sensor API.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import DummyJSON from "./components/DummyJSON";
 import GeneratePDFData from "./components/GeneratePDFData";
 import InlineCSSDemo from "./components/InlineCSSDemo";
 import JSONPlaceholder from "./components/JSONPlaceholder";
+import AirCleaner from "./components/YukaAPI";
 
 function App() {
   return (
     <>
-      <DummyJSON />
-      <InlineCSSDemo />
-      <JSONPlaceholder />
-      <GeneratePDFData />
+      {/* <DummyJSON /> */}
+      {/* <InlineCSSDemo /> */}
+      {/* <JSONPlaceholder /> */}
+      {/* <GeneratePDFData /> */}
+      {/* <AirCleaner /> */}
     </>
   );
 }
